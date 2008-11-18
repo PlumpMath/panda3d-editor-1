d48f8804681e8fbd1e9cfa1e3cfadf13fe558ec7
niertie1 <niertie1@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-11-18 14:43:22 +0000

    Initial revision

---
+++ src/main.py
@@ -0,0 +1,10 @@
+__all__ = []
+
+USE_GUI = "dgui"
+
+if __name__ == "__main__":
+    if USE_GUI == "dgui":
+        # Reto, insert stuff here
+    elif USE_GUI == "wxgui":
+        # pro-rsoft, insert stuff here
+
