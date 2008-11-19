d78f067e9b5c7e44c5147f890ceb68d38dfbb356
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-11-19 16:24:39 +0000

    reordered wx-gui startup again, now it works when the gui is started after the editor is created

--- src/main.py
+++ src/main.py
@@ -13,20 +13,20 @@ if __name__ == "__main__":
   from direct.showbase.ShowBase import ShowBase
   ShowBase()
   
-  # wxgui needs to be opened before the editor, as it open the window
-  if USE_GUI == "wxgui":
-    from wxgui.pEditorApp import EditorApp
-    app = EditorApp()
-  
   from core.pMain import EditorClass
   editor = EditorClass( render )
-  editor.loadEggModelsFile( 'testModelsFile' )
-  editor.toggle( True )
+  editor.loadEggModelsFile( 'testModelsFile.egg' )
   
   if USE_GUI == "dgui":
     from dgui.pEditorApp import EditorApp
     app = EditorApp( editor )
     app.toggle( True )
+  # wxgui needs to be opened before the editor, as it open the window
+  elif USE_GUI == "wxgui":
+    from wxgui.pEditorApp import EditorApp
+    app = EditorApp( editor )
+  
+  editor.toggle( True )
   
   run()
 
