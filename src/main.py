d846bc922e7b79ade2526ce601d978d5363f5348
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-12-17 23:44:54 +0000

    dgui did not start anymore

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ if __name__ == "__main__":
   from optparse import OptionParser
   parser = OptionParser()
   parser.add_option("-d", "--dgui",  action="store_true", dest="usedgui",  default=False, help="use the DirectGUI editor interface")
-  parser.add_option("-w", "--wxgui", action="store_true", dest="usewxgui", default=True,  help="use the wxWidgets editor interface")
+  parser.add_option("-w", "--wxgui", action="store_true", dest="usewxgui", default=False, help="use the wxWidgets editor interface")
   parser.add_option("-n", "--nogui", action="store_true", dest="usenogui", default=False, help="don't use any editor interface")
   options = parser.parse_args()[0]
   USE_GUI = None
   if options.usewxgui:  USE_GUI = "wxgui"
   elif options.usedgui: USE_GUI = "dgui"
+  print "I: main: using interface:", USE_GUI
   
   # First phase: load the configurations.
   if USE_GUI == "dgui":
