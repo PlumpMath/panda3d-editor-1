57ff274dc54cf4a19ff48803aa83af533711b891
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-12-19 14:01:38 +0000

    initial upload

---
+++ src/core/pSoundManager.py
@@ -0,0 +1,39 @@
+import traceback
+
+from direct.showbase import Audio3DManager
+from pandac.PandaModules import *
+
+class SoundManager:
+  def __init__(self):
+    base.enableAllAudio()
+    base.enableMusic(True) #bEnableMusic)
+    base.enableSoundEffects(True) #bEnableSoundEffects)
+    self.sound3dListener = NodePath('3dSoundListener')
+    self.sound3dListener.reparentTo(render)
+    self.sound3d = Audio3DManager.Audio3DManager(base.sfxManagerList[0], self.sound3dListener)
+    #self.soundMusic = base.sfxManagerList[1]
+  
+  def stuff(self):
+    # this is not actually used, it's here for reference, what features we may implement
+    '''
+    self.sound3d.setSoundVelocity(sound,velocityVector)
+    self.sound3d.setListenerVelocity(velocityVector)
+    self.sound3d.setDistanceFactor(scale)
+    self.sound3d.setDropOffFactor(scale)
+    '''
+  
+  def createListener(self):
+    pass
+  
+  def get3dManager(self):
+    return self.sound3d
+  
+  def setDoppler(self, sound):
+    try:
+      self.sound3d.setSoundVelocityAuto(sound)
+      self.sound3d.setListenerVelocityAuto()
+    except:
+      print "W: SoundManager.setDoppler: sound invalid"
+      traceback.print_exc()
+
+soundManager = SoundManager()
