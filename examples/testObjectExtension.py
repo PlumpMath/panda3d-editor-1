44d2d0cea0884277b51d67528679a51cffb66221
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-12-21 13:33:33 +0000

    updated

--- examples/testObjectExtension.py
+++ examples/testObjectExtension.py
@@ -5,32 +5,38 @@ from core.pModelIdManager import modelIdManager
 # the classname must match the filename (with a capital starting letter!)
 class TestObjectExtension(DirectObject):
   def __init__(self, object):
-    # this MUST be called, else this instance will be deleted immediately
-    # after the init has finished
-    #CodeNodeWrapper.__init__(self, name, parent)
-    # now you can do whatever you want
+    # you should store the object :-)
     self.object = object
     taskMgr.add(self.updateTask, 'updateTask-%s' % self.object.id)
     print "I: TestObjectExtension.__init__"
     print "  - searching for object", modelIdManager.getObject(self.object.id)
     print "  - number of objects in scene", len(modelIdManager.getAllModels())
+    allModels = modelIdManager.getAllModels()
+    for model in allModels:
+      try:
+        if model.getName() == 'teapot.egg.pz':
+          print "  - found teapot", model
+      except:
+        print "ERRRRRORRRR: this is a bug in the code, delete seems not have run on all objects"
     
     self.accept('arrow_up', self.up)
     print "  - done"
   
+  def destroy(self):
+    taskMgr.remove('updateTask-%s' % self.object.id)
+  
   def __del__(self):
-    #print "I: TestObjectExtension.__del__"
     self.destroy()
   
   def updateTask(self, task):
     #print "I: TestObjectExtension.update", self
-    #base.cam.lens.isInView()
-    self.object.setH( task.time * 5 )
-    return task.cont
+    if not task.isRemoved():
+      try:
+        self.object.setR( task.time * 20 )
+        return task.cont
+      except:
+        print "ERRRRRORRRR: this is a bug in the code, delete seems not have run on all objects"
   
   def up(self):
     self.object.setX(self.object, 0.5)
   
-  def destroy(self):
-    print "I: TestObjectExtension.destroy"
-    taskMgr.remove('updateTask-%s' % self.object.id)
