0ae79f31fbebb31d1f9749ea9ae5f5433019d262
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-12-21 10:55:51 +0000

    added creating of sounds

---
+++ src/dgui/modules/pSoundNodeWrapper.py
@@ -0,0 +1,10 @@
+from dgui.modules.pBaseWrapper import *
+
+class SoundNodeWrapper(BaseWrapper):
+  def __init__(self, *args, **kwargs):
+    BaseWrapper.__init__(self, *args, **kwargs)
+    self.mutableParametersSorting.append('loop')
+    self.mutableParametersSorting.append('loopCount')
+    self.mutableParametersSorting.append('volume')
+    self.mutableParametersSorting.append('playRate')
+    self.mutableParametersSorting.append('priority')
