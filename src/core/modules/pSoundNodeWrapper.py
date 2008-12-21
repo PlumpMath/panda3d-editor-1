e4319f370dc33b4888ce42599797f79a63ff8a0e
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-12-21 10:53:50 +0000

    first working version

--- src/core/modules/pSoundNodeWrapper.py
+++ src/core/modules/pSoundNodeWrapper.py
@@ -5,7 +5,20 @@ from core.pModelController import modelController
 from core.pConfigDefs import *
 from core.pSoundManager import soundManager
 
+DEBUG = False
+
 class SoundNodeWrapper(VirtualNodeWrapper):
+  def onCreateInstance(self, parent, filepath):
+    # create instance of this class
+    if filepath is not None:
+      name = filepath.split('/')[-1]
+    else:
+      name = 'NodePath'
+    objectInstance = super(SoundNodeWrapper, self).onCreateInstance(parent, name)
+    objectInstance.setSound(filepath)
+    return objectInstance
+  onCreateInstance = classmethod(onCreateInstance)
+  
   def __init__(self, parent, name):
     VirtualNodeWrapper.__init__(self, parent, name, SOUNDNODE_WRAPPER_DUMMYOBJECT)
     
@@ -13,38 +26,98 @@ class SoundNodeWrapper(VirtualNodeWrapper):
     self.soundFilepath = None
     
     self.mutableParameters['volume']    = [ float,
-      self.soundEffect.getVolume,
-      self.soundEffect.setVolume,
+      self.getVolume,
+      self.setVolume,
       None,
       None ]
-    self.mutableParameters['loop']      = [ bool,
-      self.soundEffect.getLoop,
-      self.soundEffect.setLoop,
+    '''self.mutableParameters['loop']      = [ bool,
+      self.getLoop,
+      self.setLoop,
       None,
       None ]
     self.mutableParameters['loopCount'] = [ int,
-      self.soundEffect.getLoopCount,
-      self.soundEffect.setLoopCount,
+      self.getLoopCount,
+      self.setLoopCount,
       None,
-      None ]
+      None ]'''
     self.mutableParameters['playRate']  = [ float,
-      self.soundEffect.getPlayRate,
-      self.soundEffect.setPlayRate,
+      self.getPlayRate,
+      self.setPlayRate,
       None,
       None ]
     self.mutableParameters['priority']  = [ int,
-      self.soundEffect.getPriority,
-      self.soundEffect.setPriority,
+      self.getPriority,
+      self.setPriority,
       None,
       None ]
   
-  def setSound(self, filepath):
-    self.soundFilepath = filepath
+  def getVolume(self, *args, **kwargs):
+    self.soundEffect.getVolume(*args, **kwargs)
+  def setVolume(self, *args, **kwargs):
+    self.soundEffect.setVolume(*args, **kwargs)
+  
+  '''def getLoop(self, *args, **kwargs):
+    self.soundEffect.getLoop(*args, **kwargs)
+  def setLoop(self, *args, **kwargs):
+    self.soundEffect.setLoop(*args, **kwargs)
+  
+  def getLoopCount(self, *args, **kwargs):
+    self.soundEffect.getLoopCount(*args, **kwargs)
+  def setLoopCount(self, *args, **kwargs):
+    self.soundEffect.setLoopCount(*args, **kwargs)'''
+  
+  def getPlayRate(self, *args, **kwargs):
+    self.soundEffect.getPlayRate(*args, **kwargs)
+  def setPlayRate(self, *args, **kwargs):
+    self.soundEffect.setPlayRate(*args, **kwargs)
+  
+  def getPriority(self, *args, **kwargs):
+    self.soundEffect.getPriority(*args, **kwargs)
+  def setPriority(self, *args, **kwargs):
+    self.soundEffect.setPriority(*args, **kwargs)
+  
+  def play(self, *args, **kwargs):
+    self.soundEffect.play()
+  
+  def setSound(self, soundFilepath):
+    # if there is already a model defined, remove it
+    if self.soundEffect is not None:
+      self.soundEffect.remove()
+    
+    if soundFilepath is not None:
+      filepath = str(Filename.fromOsSpecific(soundFilepath))
+      # add the model path to the panda-path
+      pandaPath = None
+      from pandac.PandaModules import getModelPath
+      for searchPath in str(getModelPath()).split():
+        if searchPath == filepath:
+          pandaPath = searchPath
+      if pandaPath is None:
+        pandaPath = '/'.join(filepath.split('/')[:-1])
+        from pandac.PandaModules import getModelPath
+        if DEBUG:
+          print "I: NodePathWrapper.setModel: adding to pandapath:"
+          print "  -", pandaPath
+        getModelPath().appendPath(pandaPath)
+      
+      # the path to the model we handle
+      self.soundFilepath = soundFilepath
+      # load the model
+      self.soundEffect = soundManager.get3dManager().loadSfx(soundFilepath)
+    
+    # if the model loading fails or no path given, use a dummy object
+    if self.soundEffect is None:
+      print "W: NodePathWrapper.setModel: model could not be loaded, loading dummy"
+      self.soundEffect = soundManager.get3dManager().loadSfx(SOUND_NOT_FOUND_SOUND)
+    # make the model visible
+    soundManager.get3dManager().attachSoundToObject( self.soundEffect, self )
+    
+    self.soundEffect.setLoop(True)
+    self.soundEffect.play()
   
   def destroy(self):
     # destroy this object
-    render.clearLight(self.lightNodePath)
-    self.lightNodePath.detachNode()
+    print "WARNING: SoundNodeWrapper.destroy not implemented !!!!"
     VirtualNodeWrapper.destroy(self)
   
   def getSaveData(self, relativeTo):
