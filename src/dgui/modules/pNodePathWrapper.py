05fbcb43cad23fcba7baa25eff632f9607481289
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-12-23 18:44:51 +0000

    removed painting mode in dgui nodepath

--- src/dgui/modules/pNodePathWrapper.py
+++ src/dgui/modules/pNodePathWrapper.py
@@ -1,15 +1,4 @@
 from dgui.modules.pBaseWrapper import BaseWrapper
-from core.pTexturePainter import texturePainter, getTextureAndStage
 
 class NodePathWrapper(BaseWrapper):
-  def startEdit(self):
-    BaseWrapper.startEdit(self)
-    
-    texStages = getTextureAndStage(self.object.model)
-    
-    if len(texStages) > 0:
-      texturePainter.startEdit(self.object.model, texStages[0][1])
-  
-  def stopEdit(self):
-    texturePainter.stopEdit()
-    BaseWrapper.stopEdit(self)
+  pass
