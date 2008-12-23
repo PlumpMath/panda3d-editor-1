c7ebe3c1a7ea7e10ed52277d69b5f5e74cf07d01
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2008-12-23 11:06:09 +0000

    test version for painter

--- src/dgui/modules/pNodePathWrapper.py
+++ src/dgui/modules/pNodePathWrapper.py
@@ -1,4 +1,17 @@
 from dgui.modules.pBaseWrapper import BaseWrapper
+from core.pTexturePainter import texturePainter, getTextureAndStage
 
 class NodePathWrapper(BaseWrapper):
-  pass
+  def startEdit(self):
+    BaseWrapper.startEdit(self)
+    
+    texStages = getTextureAndStage(self.object.model)
+    
+    self.paintModel = None
+    if len(texStages) > 0:
+      self.paintModel = texturePainter.startEdit(self.object.model, texStages[0][1])
+  
+  def stopEdit(self):
+    if self.paintModel:
+      texturePainter.stopEdit(self.paintModel)
+    BaseWrapper.stopEdit(self)
