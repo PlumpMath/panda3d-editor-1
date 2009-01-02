827521d0d865d78ae2fc80ecc3a87cdc09899daf
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2009-01-02 17:45:26 +0000

    bugfix

--- src/core/pSoundManager.py
+++ src/core/pSoundManager.py
@@ -5,32 +5,39 @@ from pandac.PandaModules import *
 
 class SoundManager:
   def __init__(self):
-    base.enableAllAudio()
-    base.enableMusic(True) #bEnableMusic)
-    base.enableSoundEffects(True) #bEnableSoundEffects)
-    self.sound3dListener = NodePath('3dSoundListener')
-    self.sound3dListener.reparentTo(render)
-    self.sound3d = Audio3DManager.Audio3DManager(base.sfxManagerList[0], self.sound3dListener)
+    self.enabled = False
   
-  def stuff(self):
-    # this is not actually used, it's here for reference, what features we may implement
-    '''
-    self.sound3d.setDistanceFactor(scale)
-    self.sound3d.setDropOffFactor(scale)
-    '''
+  def enable(self):
+    if not self.enabled:
+      base.enableAllAudio()
+      base.enableMusic(True) #bEnableMusic)
+      base.enableSoundEffects(True) #bEnableSoundEffects)
+      self.sound3dListener = NodePath('3dSoundListener')
+      self.sound3dListener.reparentTo(render)
+      self.sound3d = Audio3DManager.Audio3DManager(base.sfxManagerList[0], self.sound3dListener)
+    
+    self.enabled = True
+  
+  def disable(self):
+    self.enabled = False
   
   def createListener(self):
     pass
   
   def get3dManager(self):
-    return self.sound3d
+    if self.enabled:
+      return self.sound3d
+    print "E: SoundManager.get3dManager: is not enabled"
   
   def setDoppler(self, sound):
-    try:
-      self.sound3d.setSoundVelocityAuto(sound)
-      self.sound3d.setListenerVelocityAuto()
-    except:
-      print "W: SoundManager.setDoppler: sound invalid"
-      traceback.print_exc()
+    if self.enabled:
+      try:
+        self.sound3d.setSoundVelocityAuto(sound)
+        self.sound3d.setListenerVelocityAuto()
+      except:
+        print "W: SoundManager.setDoppler: sound invalid"
+        traceback.print_exc()
+    else:
+      print "E: SoundManager.setDoppler: is not enabled"
 
 soundManager = SoundManager()
