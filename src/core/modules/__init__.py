c5dae3c2d2531bbf0e9ed82e3e18b1d4e40e95b9
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2009-01-02 17:48:29 +0000

    import all

---
+++ src/core/modules/__init__.py
@@ -0,0 +1,14 @@
+# model nodes
+from pNodePathWrapper import *
+from pGeoMipTerrainNodeWrapper import *
+
+# light nodes
+from pAmbientLightNodeWrapper import *
+from pDirectionalLightNodeWrapper import *
+from pPointLightNodeWrapper import *
+from pSpotLightNodeWrapper import *
+
+# virtual nodes
+from pCodeNodeWrapper import *
+from pParticleSystemWrapper import *
+from pSoundNodeWrapper import *
