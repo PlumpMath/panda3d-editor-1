e9731f40e033e129dc0e20632440b6fe4d1f7f55
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2009-01-20 20:54:37 +0000

    some restructuring, new filetype for scene files

--- examples/testObjectExtension.py
+++ examples/testObjectExtension.py
@@ -2,25 +2,31 @@ from direct.showbase.DirectObject import DirectObject
 from pandac.PandaModules import *
 from core.pModelIdManager import modelIdManager
 
+DEBUG = False
+
 # the classname must match the filename (with a capital starting letter!)
 class TestObjectExtension(DirectObject):
   def __init__(self, object):
     # you should store the object :-)
     self.object = object
     taskMgr.add(self.updateTask, 'updateTask-%s' % self.object.id)
-    print "I: TestObjectExtension.__init__"
-    print "  - searching for object", modelIdManager.getObject(self.object.id)
-    print "  - number of objects in scene", len(modelIdManager.getAllModels())
-    allModels = modelIdManager.getAllModels()
-    for model in allModels:
+    if DEBUG:
+      print "I: TestObjectExtension.__init__"
+      print "  - searching for object", modelIdManager.getObject(self.object.id)
+      print "  - number of objects in scene", len(modelIdManager.getAllObjects())
+    allNodes = modelIdManager.getAllObjects()
+    for node in allNodes:
       try:
-        if model.getName() == 'teapot.egg.pz':
-          print "  - found teapot", model
+        if node.nodePath.getName() == 'teapot.egg.pz':
+          if DEBUG:
+            print "  - found teapot", node
       except:
-        print "E: this is a bug in the code, delete seems not have run on all objects"
+        if DEBUG:
+          print "E: TestObjectExtension.__init__: this is a bug in the code, delete seems not have run on all objects"
     
     self.accept('arrow_up', self.up)
-    print "  - done"
+    if DEBUG:
+      print "  - done"
   
   def destroy(self):
     taskMgr.remove('updateTask-%s' % self.object.id)
@@ -35,7 +41,8 @@ class TestObjectExtension(DirectObject):
         self.object.setR( task.time * 20 )
         return task.cont
       except:
-        print "E: this is a bug in the code, delete seems not have run on all objects"
+        if DEBUG:
+          print "E: this is a bug in the code, delete seems not have run on all objects"
   
   def up(self):
     self.object.setX(self.object, 0.5)
