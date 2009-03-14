30e8a80821a679f680c8b3544ec39c5fbf4b7f7b
tschaess <tschaess@4ca4080e-b57d-11dd-90a5-ed332e521da9>
2009-03-14 17:07:45 +0000

    adding a lot of test stuff

--- src/main.py
+++ src/main.py
@@ -17,9 +17,11 @@ if __name__ == "__main__":
   # must be as small as possible
   from core.pMain import EditorClass
   editor = EditorClass(parent=None, gui=USE_GUI)
-  editor.loadEggModelsFile("examples/mytestscene.egs")
+  #editor.loadEggModelsFile("examples/mytestscene.egs")
   #editor.loadEggModelsFile("examples/newscene.egs")
   #editor.loadEggModelsFile("examples/newscene2.egs")
+  #editor.loadEggModelsFile("examples/a.egs")
+  editor.loadEggModelsFile("examples/empty-scene.egs")
   
   editor.toggle(True)
   
